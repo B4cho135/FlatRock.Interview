b4d9d4cbc30f1e13c1a43d5c73448424d96819e4
btatarashvili <btatarashvili@BTATARASHVILI>
2024-04-16 11:05:21 +0400

    add dashboard controller

---
+++ Application/Models/StatisticsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class StatisticsModel
+    {
+        public int PerformedRecordings { get; set; }
+        public int ScreenCaptures { get; set; }
+        public int Sessions { get; set; }
+    }
+}
