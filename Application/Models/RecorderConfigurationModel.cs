1738101afafc16cb2da0d463968f02115c8e1bdb
btatarashvili <btatarashvili@BTATARASHVILI>
2024-04-16 14:08:01 +0400

    add configurator

---
+++ Application/Models/RecorderConfigurationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class RecorderConfigurationModel
+    {
+        public bool VideoCapture { get; set; }
+        public bool CapturePhoto { get; set; }
+        public int CaptureFrequency { get; set; }
+    }
+}
